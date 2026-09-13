86dca4beaf0c4d21910e8f56e494aa76e6b10333
Ajay Rajawat <ajayrajawat960@gmail.com>
2026-09-13 18:15:54 +0530

    implement utils.py

---
+++ src/utils.py
@@ -0,0 +1,60 @@
+import os
+import torch
+
+
+def save_checkpoint(model, optimizer, scheduler,
+    epoch,
+    train_loss,
+    val_loss,
+    path
+):
+    checkpoint = {
+        "epoch": epoch,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "scheduler_state_dict": (
+            scheduler.state_dict()
+            if scheduler is not None
+            else None
+        ),
+        "train_loss": train_loss,
+        "val_loss": val_loss,
+    }
+
+    os.makedirs(
+        os.path.dirname(path),
+        exist_ok=True
+    )
+
+    torch.save(checkpoint, path)
+
+
+def load_checkpoint(
+    model,
+    optimizer,
+    scheduler,
+    path,
+    device
+):
+    checkpoint = torch.load(
+        path,
+        map_location=device
+    )
+
+    model.load_state_dict(
+        checkpoint["model_state_dict"]
+    )
+
+    optimizer.load_state_dict(
+        checkpoint["optimizer_state_dict"]
+    )
+
+    if (
+        scheduler is not None
+        and checkpoint["scheduler_state_dict"] is not None
+    ):
+        scheduler.load_state_dict(
+            checkpoint["scheduler_state_dict"]
+        )
+
+    return checkpoint
